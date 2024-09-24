8b3c24118a514a731ea2d1f9af454cb6758aca65
Ibrokhimbek <ibrokhim1348@gmail.com>
2024-09-24 17:55:24 +0500

    Lesson 1

---
+++ vueapp/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  data() {
+    return {
+      user: "Ibrohim Turaboyev",
+      number: 5,
+      link: "https://t.me/IbrohimTuraboev",
+      status: "error",
+      users: ["Bobur", "E'zoza", "Malika", "Hilola", "Ma'suda"],
+    };
+  },
+  methods: {
+    showUsername() {
+      alert(this.user);
+    },
+    addUser() {
+      if (this.user.trim() !== "") {
+        this.users.push(this.user);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="addUser">
+    <input type="text" placeholder="Username" v-model="user" />
+    <button type="submit">Show username</button>
+  </form>
+  <h1>Hello {{ user }}!</h1>
+  <h1 :title="user">{{ number }}</h1>
+  <ul>
+    <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+  </ul>
+  <!-- <p v-if="status === 'pending'">User is Pending</p>
+  <p v-else-if="status === 'success'">User is Success</p>
+  <p v-else>User is error</p>
+  <button @click="isActive = !isActive">Toggle user activity</button> -->
+  <!-- <p v-show="isActive">Show</p> -->
+  <!-- <a v-bind:href="link">Ibrohimbek</a> -->
+  <!-- <a :href="link">Ibrohimbek</a> -->
+  <!-- <button v-on:click="number++">+</button>
+  <button v-on:click="number--">-</button> -->
+  <!-- <button @click="number++">+</button>
+  <button @click="number--">-</button> -->
+</template>
---
+++ vueapp/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Footer</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
